13d4fdd231c260c264fd12e4437fd92249ed0a80
Nikan <nikan619@DESKTOP-3MVJNDC.localdomain>
2022-11-01 16:42:24 -0700

    commit tree 3a49f4fcb079f3ab9adef23f0e67a45f6de5a3fb

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React,{useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory,setSelectedCategory]=useState("All")
+
+  const eventHandle=(event)=>{
+    setSelectedCategory(event.target.value)
+
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={eventHandle} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
